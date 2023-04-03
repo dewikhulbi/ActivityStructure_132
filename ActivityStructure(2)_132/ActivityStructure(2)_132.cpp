74eef132e4268e50a59b560066c2678514229762
dewikhulbi <dewikhlb20@gmail.com>
2023-04-03 10:49:33 +0700

    membuat data mahaiswa

--- ActivityStructure(2)_132/ActivityStructure(2)_132.cpp
+++ ActivityStructure(2)_132/ActivityStructure(2)_132.cpp
@@ -14,3 +14,19 @@ struct Mahasiswa
 	AlamatDetail alamat;
 	int umur;
 };
+
+int main() {
+	Mahasiswa mhs;
+
+	cout << "Masukan NIM :";
+	cin >> mhs.NIM;
+	cout << "Masukan Nama :";
+	cin >> mhs.nama;
+	cout << "Alamat :";
+	cout << "\t Masukan Desa :";
+	cin >> mhs.alamat.desa;
+	cout << "\t Masukan Kota :";
+	cin >> mhs.alamat.kota;
+	cout << "Masukan Umur :";
+	cin >> mhs.umur;
+}
