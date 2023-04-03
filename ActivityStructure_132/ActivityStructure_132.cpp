f2cd6acc4bc4ca1396226c9501cffca66294031f
dewikhulbi <dewikhlb20@gmail.com>
2023-04-03 10:32:09 +0700

    membuat data mahasiswa

--- ActivityStructure_132/ActivityStructure_132.cpp
+++ ActivityStructure_132/ActivityStructure_132.cpp
@@ -17,22 +17,4 @@ int main() {
     mhs1.alamat = "Purwokerto";
     mhs1.umur = 19;
 
-    cout << "Masukan NIM :";
-    cin >> mhs2.NIM;
-    cout << "Masukan Nama :";
-    cin >> mhs2.nama;
-    cout << "Masukan Alamat :";
-    cin >> mhs2.alamat;
-    cout << "Masukan Umur :";
-    cin >> mhs2.umur;
-    
-    cout << "\nNIM = " << mhs1.NIM;
-    cout << "\nNama =" << mhs1.nama;
-    cout << "\nAlamat =" << mhs1.alamat;
-    cout << "\n Umur =" << mhs1.umur;
-
-    cout << "\n\nNIM =" << mhs2.NIM;
-    cout << "\nNama =" << mhs2.nama;
-    cout << "\nAlamat =" << mhs2.alamat;
-    cout << "\n Umur =" << mhs2.umur;
 }
