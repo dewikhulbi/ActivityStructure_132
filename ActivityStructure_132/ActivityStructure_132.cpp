36956aea799d622f2f5f5bb68aeca807355cde0c
dewikhulbi <dewikhlb20@gmail.com>
2023-04-03 10:23:06 +0700

    membuat fungsi int main

--- ActivityStructure_132/ActivityStructure_132.cpp
+++ ActivityStructure_132/ActivityStructure_132.cpp
@@ -8,3 +8,12 @@ struct mahasiswa
     string alamat;
     int umur;
 };
+
+int main() {
+    mahasiswa mhs1, mhs2;
+
+    mhs1.NIM = "20220140132";
+    mhs1.nama = "Dewi Khulbi";
+    mhs1.alamat = "Purwokerto";
+    mhs1.umur = 19;
+}
