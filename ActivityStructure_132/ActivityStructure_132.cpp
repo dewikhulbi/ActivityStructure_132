66b6eb4541cb5ae9964e02c4d336abad0f1dccc2
dewikhulbi <dewikhlb20@gmail.com>
2023-04-03 10:12:27 +0700

    Add project files.

---
+++ ActivityStructure_132/ActivityStructure_132.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+struct mahasiswa
+{
+    string NIM;
+    string nama;
+    string alamat;
+    int umur;
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
