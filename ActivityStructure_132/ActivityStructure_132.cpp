e3d698dcfe1ddf7e42fee84e6542001e6a9eac49
dewikhulbi <dewikhlb20@gmail.com>
2023-04-03 10:42:08 +0700

    membuat struct AlamatDetail

--- ActivityStructure_132/ActivityStructure_132.cpp
+++ ActivityStructure_132/ActivityStructure_132.cpp
@@ -26,4 +26,15 @@ int main() {
     cout << "Masukan Umur :";
     cin >> mhs2.umur;
 
+    cout << "\nNIM = " << mhs1.NIM;
+    cout << "\nNama =" << mhs1.nama;
+    cout << "\nAlamat =" << mhs1.alamat;
+    cout << "\n Umur =" << mhs1.umur;
+
+    cout << "\n\nNIM =" << mhs2.NIM;
+    cout << "\nNama =" << mhs2.nama;
+    cout << "\nAlamat =" << mhs2.alamat;
+    cout << "\n Umur =" << mhs2.umur;
+
+
 }
